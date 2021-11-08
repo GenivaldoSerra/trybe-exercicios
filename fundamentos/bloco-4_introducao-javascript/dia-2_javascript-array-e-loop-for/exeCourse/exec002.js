af2f47f1fccbac63425c22daa00b75403478b4f9
GenivaldoSerra <genivaldo.serra@live.com>
2021-11-08 11:35:54 -0300

    utilizando o For/Of

--- fundamentos/bloco-4_introducao-javascript/dia-2_javascript-array-e-loop-for/exeCourse/exec002.js
+++ fundamentos/bloco-4_introducao-javascript/dia-2_javascript-array-e-loop-for/exeCourse/exec002.js
@@ -2,3 +2,9 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (var indice = 0; indice < groceryList.length; indice += 1) {
   console.log(groceryList[indice])
 }
+
+// Utilizando o for/of
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nome of names) {
+  console.log(nome);
+}
