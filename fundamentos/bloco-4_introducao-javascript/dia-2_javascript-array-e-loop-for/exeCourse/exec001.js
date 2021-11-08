622455eb544568be728c0b0b81402e28c032a669
GenivaldoSerra <genivaldo.serra@live.com>
2021-11-08 11:14:19 -0300

    Inclui exercicio 01

---
+++ fundamentos/bloco-4_introducao-javascript/dia-2_javascript-array-e-loop-for/exeCourse/exec001.js
@@ -0,0 +1,20 @@
+// Questão 01
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+// Fim
+
+// Questão 02
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = 2;
+
+console.log(indexOfPortfolio);
+// fim
+
+// Questão 03
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu);
+// Fim
