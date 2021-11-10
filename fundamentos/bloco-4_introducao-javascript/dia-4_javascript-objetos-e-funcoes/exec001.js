d108fbe8a2b38fb278fdcd4647a610dcb0236120
GenivaldoSerra <genivaldo.serra@live.com>
2021-11-10 19:16:38 -0300

    Atualizando exercicios

--- fundamentos/bloco-4_introducao-javascript/dia-4_javascript-objetos-e-funcoes/exec001.js
+++ fundamentos/bloco-4_introducao-javascript/dia-4_javascript-objetos-e-funcoes/exec001.js
@@ -33,3 +33,22 @@ console.log("A jogadora possui", medals["golden"] + " medalhas de ouro e "+ meda
 
 /* Exercícios sobre For/In */
 
+// Primeira Questão
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+for(let indice in names) {
+  console.log('Olá ', names[indice]);
+}
+
+// Segunda questão
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+for (let key in car) {
+  console.log(key, car[key]);
+}
