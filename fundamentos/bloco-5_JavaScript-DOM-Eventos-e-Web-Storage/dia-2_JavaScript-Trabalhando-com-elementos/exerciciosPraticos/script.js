c938bfc4e9b236a45732a48cb9348879ad4b2494
GenivaldoSerra <genivaldo.serra@live.com>
2021-11-17 16:12:52 -0300

    Inclui exercicio pratico

---
+++ fundamentos/bloco-5_JavaScript-DOM-Eventos-e-Web-Storage/dia-2_JavaScript-Trabalhando-com-elementos/exerciciosPraticos/script.js
@@ -0,0 +1,11 @@
+// Questão - 01
+let elementH1 = document.createElement('h1');
+      elementH1.innerText = 'Exercício 5.2 - JavaScript DOM';
+      document.body.appendChild(elementH1);
+
+// Questão - 02
+let elementMain = document.createElement('main');
+      elementMain.className = 'main-content';
+      document.body.appendChild(elementMain);
+
+// Questão - 03
