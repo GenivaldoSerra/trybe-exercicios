ac263efddbe820f4901a1e6c9de82232c606f114
Genivaldo <genivaldo.serra@live.com>
2021-11-17 10:38:55 -0300

    Inclui dia 2 do bloco 5

---
+++ fundamentos/bloco-5_JavaScript-DOM-Eventos-e-Web-Storage/dia-2_JavaScript-Trabalhando-com-elementos/exercicios/script.js
@@ -0,0 +1,12 @@
+//console.log(document.getElementById('start').nextSibling)
+
+//console.log(document.getElementById('start').nextElementSibling)
+
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling)
+
+console.log(document.getElementById('elementoOndeVoceEsta').parentNode)
+
+
+
+
+
