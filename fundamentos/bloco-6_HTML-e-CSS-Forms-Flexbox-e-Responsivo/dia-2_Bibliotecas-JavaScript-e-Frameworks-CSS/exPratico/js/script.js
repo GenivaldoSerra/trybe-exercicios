83a0c6b2b0277bc1a6450c5fdde27c48268c9c8f
GenivaldoSerra <genivaldo.serra@live.com>
2021-11-26 10:57:21 -0300

    Atualizacao do Form

--- fundamentos/bloco-6_HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-2_Bibliotecas-JavaScript-e-Frameworks-CSS/exPratico/js/script.js
+++ fundamentos/bloco-6_HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-2_Bibliotecas-JavaScript-e-Frameworks-CSS/exPratico/js/script.js
@@ -1,6 +1,6 @@
 const estado = document.getElementById('estados');
 
-const listEstados = ['Rio de Janeiro', 'Piaui', 'Bahia', 'Maranhão'];
+const listEstados = ['Rio de Janeiro', 'Piauí', 'Bahia', 'Maranhão'];
 for(let index = 0; index < listEstados.length; index += 1) {
   const elementOption = document.createElement('option');
   elementOption.innerHTML = listEstados[index];
