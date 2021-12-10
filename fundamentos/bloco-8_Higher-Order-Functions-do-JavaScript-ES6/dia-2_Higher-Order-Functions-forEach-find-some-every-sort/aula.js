72d070afd5eec584df5060d06cc13d0c2357f9a0
GenivaldoSerra <genivaldo.serra@live.com>
2021-12-10 15:17:02 -0300

    Inclui exPratico

--- fundamentos/bloco-8_Higher-Order-Functions-do-JavaScript-ES6/dia-2_Higher-Order-Functions-forEach-find-some-every-sort/aula.js
+++ fundamentos/bloco-8_Higher-Order-Functions-do-JavaScript-ES6/dia-2_Higher-Order-Functions-forEach-find-some-every-sort/aula.js
@@ -75,10 +75,19 @@ console.log(firstMultipleOf5);
 // 50 */
 
 // Tabuada
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+/* const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const multipliesFor2 = (element) => {
   console.log(`${element} * 8: ${element * 8}`);
 };
 
-numbers.forEach(multipliesFor2);
+numbers.forEach(multipliesFor2); */
+
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
