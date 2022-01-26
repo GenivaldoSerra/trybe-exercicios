b2feee91abc1b22efd7bb01e7907ce1c2498fb67
Genivaldo <genivaldo.serra@live.com>
2022-01-26 15:43:36 -0300

    Atulizando exercicio

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import './App.css';
-
-function handleBtn01() {
-  console.log('Clicou no botão!');
-}
-
-const handleBtn02 = () => {
-  console.log('Usou o segundo botão!!');
-}
-
-const handleBtn03 = () => {
-  console.log('Usou o terceiro botão!!!')
-}
-
 class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.handleBtn01 = this.handleBtn01.bind(this);
+    this.handleBtn02 = this.handleBtn02.bind(this);
+    this.handleBtn03 = this.handleBtn03.bind(this);
+  }
+
+  handleBtn01() {
+    console.log('Clicou no botão!');
+  }
+  
+  handleBtn02() {
+    console.log('Usou o segundo botão!!');
+  }
+  
+  handleBtn03() {
+    console.log('Usou o terceiro botão!!!')
+  }
   render() {
     return (
       <main>
-        <button onClick={handleBtn01}>Meu botão</button>
-        <button onClick={handleBtn02}>Botão 02</button>
-        <button onClick={handleBtn03}>Botão 03</button>
+        <button onClick={this.handleBtn01}>Meu botão</button>
+        <button onClick={this.handleBtn02}>Botão 02</button>
+        <button onClick={this.handleBtn03}>Botão 03</button>
       </main>
     )
   }
